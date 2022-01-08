4baac79b53265ec3f56b6927edcbfe5eca3564e9
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-08 14:20:42 -0800

    init function console log test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
+const inquirer = require("inquirer");
+
+console.log("Welcome to the README generator.")
 
 // TODO: Create an array of questions for user input
 // title, description, table of contents (will try and do dynamically from content?), installation, usage, license, contributing, tests and questions
@@ -8,11 +11,19 @@ const questions = [
     type: "input",
     name: "title",
     message: "What is the title of your project?",
+    validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter a title for your project!');
+          return false;
+        }
+      }
   },
   {
     type: "input",
     name: "description",
-    message: "What is the description of your project?",
+    message: "Please provide a description of your project and a link to the deployed application.",
   },
   {
     type: "input",
@@ -22,7 +33,7 @@ const questions = [
   {
     type: "input",
     name: "usage",
-    message: "Provide instructions and examples for use. ",
+    message: "Provide instructions and examples for use.",
   },
   // is there a way to categorize by type and expand options from there?
   {
@@ -50,13 +61,20 @@ const questions = [
     name: "tests",
     message: "Go the extra mile and write tests for your application. Then provide examples on how to run them.",
   },
+  //include features?
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+    })
+}
 
 // Function call to initialize app
 init();
